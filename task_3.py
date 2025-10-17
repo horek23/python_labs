5623d29de0355f5531b2323ceb5a03c2558375b0
horek23 <sergeeva.alina12122006@gmail.com>
2025-10-17 10:36:11 +0300

    add task_3

---
+++ task_3.py
@@ -0,0 +1,9 @@
+print("Введите пароль: ")
+password = input()
+
+if len(password) < 16:
+    print("Слишком короткий")
+elif password.isalpha() or password.isnumeric():
+    print("Слабый пароль")
+else:
+    print("Надежный пароль")
